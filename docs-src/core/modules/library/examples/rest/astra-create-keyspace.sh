77de272765afd2cb688f8c55e87d768178dee856
Lorina Poland <lorinapoland@gmail.com>
2023-02-08 15:21:30 -0800

    initial library dm work

---
+++ docs-src/core/modules/library/examples/rest/astra-create-keyspace.sh
@@ -0,0 +1,6 @@
+curl --location --request POST 'https://{{ASTRA_DB_ID}}-{{ASTRA_DB_REGION}}.apps.astra.datastax.com:8180/v2/schemas/namespaces' \
+--header "X-Cassandra-Token: $AUTH_TOKEN" \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "{keyspace}"
+}'
